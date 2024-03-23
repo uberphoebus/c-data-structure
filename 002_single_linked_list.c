8415ecf6481b0111779c50367876e22b9e0414fe
uberphoebus <87922930+uberphoebus@users.noreply.github.com>
2024-03-23 15:28:01 +0000

    test single linked list

--- 002_single_linked_list.c
+++ 002_single_linked_list.c
@@ -10,17 +10,18 @@ int main() {
   // test node
   NODE list[5] = {0};
 
-  list[0].next = &list[1];
-  list[1].next = &list[2];
-  list[2].next = &list[3];
-  list[3].next = &list[4];
-  list[4].next = NULL;
-
   list[0].nData = 100;
   list[1].nData = 200;
   list[2].nData = 300;
   list[3].nData = 400;
   list[4].nData = 500;
+
+  // connect
+  list[0].next = &list[4];
+  list[1].next = &list[2];
+  list[2].next = &list[3];
+  list[3].next = 0;
+  list[4].next = &list[1];
 
   for (int i = 0; i < 5; ++i)  {
     printf("list[%d] : %d [%p]\n", i, list[i].nData, list[i].next);
